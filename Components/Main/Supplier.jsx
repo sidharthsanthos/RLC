7be63705fd6f9547fe4000df7608a5471e72af45
fossbin <stevedominicfez@gmail.com>
2025-11-20 14:53:01 +0530

    Remove cycle navigation

--- Components/Main/Supplier.jsx
+++ Components/Main/Supplier.jsx
@@ -1,16 +1,24 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet } from 'react-native'
 import React from 'react'
-import SupplierTabs from '../Supplier/SupplierTabs'
 import { createStackNavigator } from '@react-navigation/stack'
-import Index from '../Index';
 
 const Stack=createStackNavigator();
 
 const Supplier = () => {
   return (
     <Stack.Navigator screenOptions={{headerShown:false}}>
-        <Stack.Screen name='SupplierTabs' component={SupplierTabs}/>
-        <Stack.Screen name='Index' component={Index}/>
+      <Stack.Screen name="SupplierTabs">
+        {props => {
+          const SupplierTabs = require('../Supplier/SupplierTabs').default
+          return <SupplierTabs {...props} />
+        }}
+      </Stack.Screen>
+      <Stack.Screen name="Index">
+        {props => {
+          const Index = require('../Index').default
+          return <Index {...props} />
+        }}
+      </Stack.Screen>
     </Stack.Navigator>
   )
 }
